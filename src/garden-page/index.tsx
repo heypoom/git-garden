26ab53f2864b003627c2d62040c69672ff5e7f42
Pmc Dev <phoomparin@gmail.com>
2019-01-13 05:43:19 +0700

    :fire: Remove unused imports

--- src/garden-page/index.tsx
+++ src/garden-page/index.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useContext} from 'react'
-import {observer, useObserver, useObservable} from 'mobx-react-lite'
+import React, {useEffect} from 'react'
+import {observer} from 'mobx-react-lite'
 
 import {store} from '../store'
 import {GardenDisplay} from '../garden-display'
