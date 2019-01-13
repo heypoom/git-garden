32c8750a722426095e2a90aafdd0a480fb518b13
Pmc Dev <phoomparin@gmail.com>
2019-01-13 19:56:08 +0700

    :zap: Use svg files instead of inline svg

--- src/garden-display/getTile.ts
+++ src/garden-display/getTile.ts
@@ -1,9 +1,9 @@
 export function getTile(count: number) {
-  if (count === 0) return require('./assets/0.svg')
-  if (count >= 1 && count < 5) return require('./assets/1.svg')
-  if (count >= 5 && count < 10) return require('./assets/2.svg')
-  if (count >= 10 && count < 20) return require('./assets/3.svg')
-  if (count >= 20 && count < 30) return require('./assets/4.svg')
-  if (count >= 30 && count < 40) return require('./assets/5.svg')
-  if (count >= 40) return require('./assets/6.svg')
+  if (count === 0) return '/tiles/0.svg'
+  if (count >= 1 && count < 5) return '/tiles/1.svg'
+  if (count >= 5 && count < 10) return '/tiles/2.svg'
+  if (count >= 10 && count < 20) return '/tiles/3.svg'
+  if (count >= 20 && count < 30) return '/tiles/4.svg'
+  if (count >= 30 && count < 40) return '/tiles/5.svg'
+  if (count >= 40) return '/tiles/6.svg'
 }
